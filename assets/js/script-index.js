567a394d506c65a6b4054532472fafbdc41429b0
Djendo <HoBeOne@MacBook-Pro-de-Djendo.local>
2019-02-21 15:51:38 +0100

    final commit

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,6 +1,15 @@
 
 
   	var nom = prompt("Quel est votre nom ?", name);
-	window.document.getElementById("target-1").innerHTML = "<b>" + nom + "</b>";
-	window.document.getElementById("target").innerHTML = "Bonjour et bienvenue : " + nom;
+  	
+  	if(nom == null){
+	  
+	  window.document.getElementById("target-1").innerHTML = "";
+	  window.document.getElementById("target").innerHTML = "Bonjour et bienvenue";
+	
+  	}
+	else{
+		  window.document.getElementById("target-1").innerHTML = "<b>" + nom + "</b>";
+	window.document.getElementById("target").innerHTML = "Bonjour et bienvenue : " + nom;	
+	}
 
